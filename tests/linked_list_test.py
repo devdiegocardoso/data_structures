c29f3d9f56177da5e83ee7b8785bcfa7899f7ca6
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-03-27 16:36:00 +0200

    update: error messages in tests updated

--- tests/linked_list_test.py
+++ tests/linked_list_test.py
@@ -13,40 +13,40 @@ class TestLinkedList(unittest.TestCase):
     def test_add_single(self):
         lista = List()
         lista.add(1)
-        self.assertEqual(lista.get_list(),"1","Nao adicionou corretamente")
+        self.assertEqual(lista.get_list(),"1","Value was not added correctly.")
     def test_add(self):
         lista = List()
         lista.add(1)
         lista.add(2)
-        self.assertEqual(lista.get_list(),"1 -> 2","Nao adicionou corretamente")
+        self.assertEqual(lista.get_list(),"1 -> 2","Value was not added correctly.")
     def test_remove_begin(self):
         lista = List()
         lista.add(1)
         lista.add(2)
         lista.add(3)
         lista.remove(1)
-        self.assertEqual(lista.get_list(),"2 -> 3","Nao removeu corretamente")
+        self.assertEqual(lista.get_list(),"2 -> 3","Value was not removed correctly.")
     def test_remove_middle(self):
         lista = List()
         lista.add(1)
         lista.add(2)
         lista.add(3)
         lista.remove(2)
-        self.assertEqual(lista.get_list(),"1 -> 3","Nao removeu corretamente")
+        self.assertEqual(lista.get_list(),"1 -> 3","Value was not removed correctly.")
     def test_remove_end(self):
         lista = List()
         lista.add(1)
         lista.add(2)
         lista.add(3)
         lista.remove(3)
-        self.assertEqual(lista.get_list(),"1 -> 2","Nao removeu corretamente")
+        self.assertEqual(lista.get_list(),"1 -> 2","Value was not removed correctly.")
     def test_remove_nonexistent(self):
         lista = List()
         lista.add(1)
         lista.add(2)
         lista.add(3)
         lista.remove(4)
-        self.assertEqual(lista.get_list(),"1 -> 2 -> 3","Nao removeu corretamente")
+        self.assertEqual(lista.get_list(),"1 -> 2 -> 3","Value must not be removed.")
 
 if __name__ == '__main__':
     unittest.main()
